9d6fe86d15f0292d8d28a06da82abaac4aef5a1e
fatima <fatima.b.jefjaf@gmail.com>
2025-07-01 10:26:04 +0200

    refactor: moving members data in file data/members.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import MemberCard from './components/MemberCard';
+import { members } from './data/members';
 
 
 
@@ -10,40 +11,14 @@ function App() {
       <div>
         <h2>Liste des membres</h2>
         <div className='cards-container'>
-          <MemberCard 
-            firstName="Inès" 
-            lastName="Dupont" 
-            tech="React" 
-            message="Toujours partante pour un nouveau challenge frontend !" 
-          />
-
-          <MemberCard 
-            firstName="Liam" 
-            lastName="Nguyen" 
-            tech="Laravel" 
-            message="Le back c’est ma passion, mais j’apprivoise React petit à petit." 
-          />
-
-          <MemberCard 
-            firstName="Amina" 
-            lastName="Benali" 
-            tech="Vue.js" 
-            message="Pro du CSS, j’adore soigner les détails visuels." 
-          />
-
-          <MemberCard 
-            firstName="Noé" 
-            lastName="Durand" 
-            tech="Node.js" 
-            message="Le JS côté serveur, c’est mon truc. Je suis ici pour monter en fullstack." 
-          />
-
-          <MemberCard 
-            firstName="Clara" 
-            lastName="Morel" 
-            tech="Python" 
-            message="J’ai un faible pour les API propres et bien documentées." 
-          />
+          {members.map((member) => (
+            <MemberCard
+              key={JSON.stringify(member)}
+              name={`${member.firstName} ${member.lastName}`}
+              tech={member.tech}
+              message={member.message}
+            />
+          ))}
         </div>
       </div>
     </div>
